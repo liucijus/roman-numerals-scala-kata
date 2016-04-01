4bdffe0f9abfddb6bf96ad8821fb6b27af2cce1c
Vaidas Pilkauskas <vaidasp@wix.com>
2016-04-01 15:01:32 +0300

    Add test for 2 to II conversion

--- src/test/scala/RomanNumeralsConverterTest.scala
+++ src/test/scala/RomanNumeralsConverterTest.scala
@@ -4,8 +4,12 @@ class RomanNumeralsConverterTest extends FlatSpec with Matchers {
   "Roman numerals converter" should "convert 1 number to I" in {
     RomanNumeralsConverter.fromArabic(1) shouldBe "I"
   }
+
+  it should "convert 2 to II" in {
+    RomanNumeralsConverter.fromArabic(2) shouldBe "II"
+  }
 }
 
 object RomanNumeralsConverter {
-  def fromArabic(number: Int): String = "I"
+  def fromArabic(number: Int): String = if (number == 1) "I" else "II"
 }
